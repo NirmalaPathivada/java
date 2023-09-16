9e5b8ccaee0f00900f58ec22f2de12fb6b4db3cf
NirmalaPathivada <143801923+NirmalaPathivada@users.noreply.github.com>
2023-09-16 20:22:28 +0530

    Add files via upload

---
+++ Scannerfunction.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Scannerfunction {
+
+	public static void main(String[] args) {
+	   Scanner ojb=new Scanner(System.in);
+	   System.out.print("enter the nuber");
+	    int number=ojb.nextInt();
+	    
+	    System.out.println(number);
+		
+
+	}
+
+}
